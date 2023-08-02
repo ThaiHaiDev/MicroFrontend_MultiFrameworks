3eb1742cfe7a25fb2d7e37441f7b36f243283b5c
ThaiHaiDev <thaihai1004@gmail.com>
2023-08-03 00:07:40 +0700

    create project angular

---
+++ apps/angular-host/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'check-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-host';
+}
