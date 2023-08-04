b04b0ef9057414e004eb5b477271bd3c1a445b43
ThaiHaiDev <thaihai1004@gmail.com>
2023-08-04 17:42:21 +0700

    create project nagular-remote

--- apps/angular-host/src/app/app.routes.ts
+++ apps/angular-host/src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'angular-remote',
+    loadChildren: () =>
+      import('angular-remote/Module').then((m) => m.RemoteEntryModule),
+  },
+  {
     path: '',
     component: NxWelcomeComponent,
   },
