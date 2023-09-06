7c9e26f0c6a8a5925820ba6d731827cd1f8a0e9b
ThaiHaiDev <thaihai1004@gmail.com>
2023-09-06 15:33:16 +0700

    create libs for react with mui v5

--- apps/react-remote/src/app/app.tsx
+++ apps/react-remote/src/app/app.tsx
@@ -1,12 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Address } from '@check/address';
 import styles from './app.module.scss';
-
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
       <NxWelcome title="react-remote" />
+      <Address />
     </div>
   );
 }
---
+++ libs/address/src/lib/address.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+import styles from './address.module.scss';
+
+/* eslint-disable-next-line */
+export interface AddressProps {}
+
+export function Address(props: AddressProps) {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <div>
+      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="demo-select-small-label">Age</InputLabel>
+      <Select
+        labelId="demo-select-small-label"
+        id="demo-select-small"
+        value={age}
+        label="Age"
+        onChange={handleChange}
+      >
+        <MenuItem value="">
+          <em>None</em>
+        </MenuItem>
+        <MenuItem value={10}>Ten</MenuItem>
+        <MenuItem value={20}>Twenty</MenuItem>
+        <MenuItem value={30}>Thirty</MenuItem>
+      </Select>
+    </FormControl>
+    </div>
+  );
+}
+
+export default Address;
